5343328a488d4556c8273eb09b24ed8e8e8f90cc
ljpetkovic <ljudmila.petkovic@gmail.com>
2021-11-28 18:50:17 +0100

    comment the code

--- ajouter_contenu.py
+++ ajouter_contenu.py
@@ -3,6 +3,6 @@
 fichier = open('bonjour.txt','a')         # ouvrir un fichier pré-existant pour y ajouter du contenu avec le paramètre 'a' (append)
                                           # si nous avions ouvert ce fichier en mode écriture avec le paramètre 'w'
                                           # et si nous avions utilisé la méthode 'write',
-                                          # le contenu précedent sera écrasé
+                                          # le contenu précedent aurait été écrasé
 fichier.write('\n' + 'Vous allez bien ?') 
 fichier.close()
