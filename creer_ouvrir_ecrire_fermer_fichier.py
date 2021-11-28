d8303e59e918a8c354fa1439f391b70633ce42c4
ljpetkovic <ljudmila.petkovic@gmail.com>
2021-11-28 18:31:31 +0100

    comment code

--- creer_ouvrir_ecrire_fermer_fichier.py
+++ creer_ouvrir_ecrire_fermer_fichier.py
@@ -1,6 +1,7 @@
 ### Enregistrer le texte créé dans un fichier du disque dur
 
 fichier = open('bonjour.txt','w') # créer et ouvrir un fichier intitulé 'bonjour'
+                                  # 'open' et 'close' sont des méthodes sur fichier (file methods) qui agissent sur l'objet 'fichier'
                                   # paramètre 'w' spécifie que l'on va écrire ('write') du contenu dans ce fichier
 fichier.write('Bonjour !')
 fichier.close()
