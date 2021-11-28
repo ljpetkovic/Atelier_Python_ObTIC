c52fee570169739a8b6c46586d6eac948bdbd0e1
ljpetkovic <ljudmila.petkovic@gmail.com>
2021-11-28 18:52:20 +0100

    comment the code

--- ajouter_contenu.py
+++ ajouter_contenu.py
@@ -1,5 +1,9 @@
 ### Ajouter du contenu à un fichier texte existant
 
+# si nous lançons ce script plusieurs fois, nous verrons que plusieurs lignes
+# contenant le message 'Vous allez bien ?' sont apparues
+
+
 fichier = open('bonjour.txt','a')         # ouvrir un fichier pré-existant pour y ajouter du contenu avec le paramètre 'a' (append)
                                           # si nous avions ouvert ce fichier en mode écriture avec le paramètre 'w'
                                           # et si nous avions utilisé la méthode 'write',
